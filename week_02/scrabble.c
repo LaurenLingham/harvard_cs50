70ff3bcd57c9067457b5ecb2a6b308b997bd378f
LaurenLingham <lauren.lingham@outlook.com>
2023-06-19 12:23:04 +0100

    prints result fo player 1 win

--- week_02/scrabble.c
+++ week_02/scrabble.c
@@ -18,7 +18,11 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
 }
 
 int compute_score(string word)
