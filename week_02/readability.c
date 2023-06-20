5c2a841f14a927e7627421cb2d7ad9e427bb11ef
LaurenLingham <lauren.lingham@outlook.com>
2023-06-20 14:31:08 +0100

    added notes

--- week_02/readability.c
+++ week_02/readability.c
@@ -8,7 +8,7 @@ int count_sentences(string text);
 
 int main(void)
 {
-    // Asks user for text and prints it to the console
+    // Asks user for text and prints it to the console, along with number of letters, words & sentences
     string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
@@ -21,6 +21,7 @@ int count_letters(string text)
 {
     int count = 0;
 
+    //for each index of the string, if the character is between a-z (both lower & upper case), add 1 to the letter count
     for (int i = 0; i < strlen(text); i++)
     {
         if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
@@ -34,19 +35,22 @@ int count_words(string text)
 {
     int count = 0;
 
+    // for each index of the string, if the character is a space add 1 to the word count
     for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] == ' '0)
+        if (text[i] == ' ')
             count ++;
     }
 
-    return count;
+    // add 1 to the count total to include the final word
+    return count + 1;
 }
 
 int count_sentences(string text)
 {
     int count = 0;
 
+    // for each index of the string, if the character is a period, exclamation or question mark, add 1 to the sentence count
     for (int i = 0; i < strlen(text); i++)
     {
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
