2e89de181f871865c49c9e21040b1993feaf656f
LaurenLingham <lauren.lingham@outlook.com>
2023-06-20 14:19:56 +0100

    added sentence count function

--- week_02/readability.c
+++ week_02/readability.c
@@ -4,6 +4,7 @@
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
@@ -11,8 +12,9 @@ int main(void)
     string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
+    int sentences = count_sentences(text);
 
-    printf("%s\n %i letters\n %i words\n", text, letters, words);
+    printf("%s\n %i letters\n %i words\n %i sentences\n", text, letters, words, sentences);
 }
 
 int count_letters(string text)
@@ -39,4 +41,9 @@ int count_words(string text)
     }
 
     return count;
+}
+
+int count_sentences(string text)
+{
+    
 }
