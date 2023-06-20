368edb4806c0ae6cbdc3950cf16324ff42d6e7bb
LaurenLingham <lauren.lingham@outlook.com>
2023-06-20 11:59:53 +0100

    function added for count letters

--- week_02/readability.c
+++ week_02/readability.c
@@ -1,9 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text);
+
 int main(void)
 {
+    // Asks user for text and prints it to the console
     string text = get_string("Text: ");
 
     printf("%s\n", text);
+}
+
+int count_letters(string text)
+{
+    
 }
