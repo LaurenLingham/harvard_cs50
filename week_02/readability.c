63ec6a87185cb7541e24f35a2517f907eb0c2408
LaurenLingham <lauren.lingham@outlook.com>
2023-06-20 12:02:07 +0100

    letters variable added

--- week_02/readability.c
+++ week_02/readability.c
@@ -7,8 +7,9 @@ int main(void)
 {
     // Asks user for text and prints it to the console
     string text = get_string("Text: ");
+    int letters = count_letters(text);
 
-    printf("%s\n", text);
+    printf("%s\n %i letters", text, letters);
 }
 
 int count_letters(string text)
