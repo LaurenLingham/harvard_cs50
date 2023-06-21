452b8b57d5f5c45eb71be72bad0193f41847b540
LaurenLingham <lauren.lingham@outlook.com>
2023-06-21 12:21:33 +0100

    converts plaintext to ciphertext

--- week_02/caeser.c
+++ week_02/caeser.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
     // check for correct number of arguments
     if (argc != 2)
@@ -13,9 +14,9 @@ int main(int argc, string argv[])
     }
 
     // check key is positive integer
-    for (int i=0; i < strlen(argv[1]); i++)
+    for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if(isdigit(argv[1][i]))
+        if (!isdigit(argv[1][i]))
         {
             printf("Usage: ./caesar key\n");
             return 1;
@@ -23,11 +24,29 @@ int main(int argc, string argv[])
     }
 
     // convert key to integer
-    int k = atoi(argv[i]);
+    int k = atoi(argv[1]);
 
     // request input from user then change to ciphertext
     string plaintext = get_string("plaintext: ");
     printf("ciphertext: ");
-    
-    // 
+
+    // loop through each character of input and if it's a letter swap it using the key
+    for (int j = 0; j < strlen(plaintext); j++)
+    {
+        if (isupper(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
+        }
+        else if (islower(plaintext[j]))
+        {
+            printf("%c", (plaintext[j] - 97 + k) % 26 + 97);
+        }
+        else
+        {
+            printf("%c", plaintext[j]);
+        }
+    }
+
+    //print a new line
+    printf("\n");
 }
